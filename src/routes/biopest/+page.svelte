46112c21b026155563e3bec79272d5c789be857e
aris1abadi <aris1abadi@gmail.com>
2024-11-17 14:09:43 +0700

    modified:   src/routes/biopest/+page.svelte 	modified:   src/routes/home/+page.svelte 	modified:   src/routes/pestisida/+page.svelte 	modified:   src/routes/setup/+page.svelte 	modified:   src/routes/siram/+page.svelte 	modified:   svelte.config.js

--- src/routes/biopest/+page.svelte
+++ src/routes/biopest/+page.svelte
@@ -942,10 +942,10 @@
 										on:change={() => semprotBiopest(1)}
 									/>
 									<div class="swap-on h-6 w-12">
-										<img src=" {base}/on6.png" alt="btn_on" />
+										<img src="  /on6.png" alt="btn_on" />
 									</div>
 									<div class="swap-off h-6 w-12">
-										<img src=" {base}/off6.png" alt="btn_off" />
+										<img src="  /off6.png" alt="btn_off" />
 									</div>
 								</label>
 								<label class="swap h-10 w-full text-center text-xs">
@@ -956,10 +956,10 @@
 										on:change={() => semprotBiopest(2)}
 									/>
 									<div class="swap-on h-6 w-12">
-										<img src=" {base}/on6.png" alt="btn_on" />
+										<img src="  /on6.png" alt="btn_on" />
 									</div>
 									<div class="swap-off h-6 w-12">
-										<img src=" {base}/off6.png" alt="btn_off" />
+										<img src="  /off6.png" alt="btn_off" />
 									</div>
 								</label>
 								<label class="swap h-10 w-full text-center text-xs">
@@ -970,10 +970,10 @@
 										on:change={() => semprotBiopest(3)}
 									/>
 									<div class="swap-on h-6 w-12">
-										<img src=" {base}/on6.png" alt="btn_on" />
+										<img src="  /on6.png" alt="btn_on" />
 									</div>
 									<div class="swap-off h-6 w-12">
-										<img src=" {base}/off6.png" alt="btn_off" />
+										<img src="  /off6.png" alt="btn_off" />
 									</div>
 								</label>
 								<label class="swap h-10 w-full text-center text-xs">
@@ -984,10 +984,10 @@
 										on:change={() => semprotBiopest(4)}
 									/>
 									<div class="swap-on h-6 w-12">
-										<img src=" {base}/on6.png" alt="btn_on" />
+										<img src="  /on6.png" alt="btn_on" />
 									</div>
 									<div class="swap-off h-6 w-12">
-										<img src=" {base}/off6.png" alt="btn_off" />
+										<img src="  /off6.png" alt="btn_off" />
 									</div>
 								</label>
 							</div>
@@ -1010,10 +1010,10 @@
 											on:change={() => semprotBiopest(0)}
 										/>
 										<div class="swap-on">
-											<img src=" {base}/on4.png" alt="btn_on" />
+											<img src="  /on4.png" alt="btn_on" />
 										</div>
 										<div class="swap-off">
-											<img src=" {base}/off4.png" alt="btn_off" />
+											<img src="  /off4.png" alt="btn_off" />
 										</div>
 									</label>
 								</div>
@@ -1056,37 +1056,37 @@
 
 				<ul class="w-1/2 dropdown-content z-[1] rounded shadow-lg border">
 					<li>
-						<a href="/home" class="grid grid-cols-4 bg-gray-200 p-2"
-							><img class="w-8 h-8" src="{base}/btn_home2.png" alt="home" />
+						<a href=" /home" class="grid grid-cols-4 bg-gray-200 p-2"
+							><img class="w-8 h-8" src=" /btn_home2.png" alt="home" />
 							<div class="col-span-3 p-1">Home</div></a
 						>
 					</li>
 					<li>
-						<a href="/siram" class="grid grid-cols-4 bg-white p-2"
-							><img class="w-8 h-8" src="{base}/penyiraman.png" alt="siram" />
+						<a href=" /siram" class="grid grid-cols-4 bg-white p-2"
+							><img class="w-8 h-8" src=" /penyiraman.png" alt="siram" />
 							<div class="col-span-3 p-1">Siram</div></a
 						>
 					</li>
 					<li>
-						<a href="/pestisida" class="grid grid-cols-4 bg-gray-200 p-2"
-							><img class="w-8 h-8" src="{base}/pestisida.png" alt="pest" />
+						<a href=" /pestisida" class="grid grid-cols-4 bg-gray-200 p-2"
+							><img class="w-8 h-8" src=" /pestisida.png" alt="pest" />
 							<div class="col-span-3 p-1">Pestisida</div></a
 						>
 					</li>
 					<li>
-						<a href="/biopest" class="grid grid-cols-4 bg-white p-2"
-							><img class="w-8 h-8" src="{base}/biopestisida.png" alt="biopest" />
+						<a href=" /biopest" class="grid grid-cols-4 bg-white p-2"
+							><img class="w-8 h-8" src=" /biopestisida.png" alt="biopest" />
 							<div class="col-span-3 p-1">Biopestisida</div></a
 						>
 					</li>
 					<li>
-						<a href="/setup" class="grid grid-cols-4 bg-gray-200 p-2"
-							><img class="w-8 h-8" src="{base}/setup.png" alt="home" />
+						<a href=" /setup" class="grid grid-cols-4 bg-gray-200 p-2"
+							><img class="w-8 h-8" src=" /setup.png" alt="home" />
 							<div class="col-span-3 p-1">Setup</div></a
 						>
 					</li>
 					<li>
-						<a href="/" class="grid grid-cols-4 bg-white p-2"
+						<a href=" /" class="grid grid-cols-4 bg-white p-2"
 							><svg
 								fill="#000000"
 								class="w-6 h-6"
@@ -1148,10 +1148,10 @@
 									on:change={() => jadwalAktif(1)}
 								/>
 								<div class="swap-on h-6 w-12">
-									<img src=" {base}/on6.png" alt="btn_on" />
+									<img src="  /on6.png" alt="btn_on" />
 								</div>
 								<div class="swap-off h-6 w-12">
-									<img src=" {base}/off6.png" alt="btn_off" />
+									<img src="  /off6.png" alt="btn_off" />
 								</div>
 							</label>
 						</div>
@@ -1248,10 +1248,10 @@
 									on:change={() => jadwalAktif(2)}
 								/>
 								<div class="swap-on h-6 w-12">
-									<img src=" {base}/on6.png" alt="btn_on" />
+									<img src="  /on6.png" alt="btn_on" />
 								</div>
 								<div class="swap-off h-6 w-12">
-									<img src=" {base}/off6.png" alt="btn_off" />
+									<img src="  /off6.png" alt="btn_off" />
 								</div>
 							</label>
 						</div>
@@ -1348,10 +1348,10 @@
 									on:change={() => jadwalAktif(3)}
 								/>
 								<div class="swap-on h-6 w-12">
-									<img src=" {base}/on6.png" alt="btn_on" />
+									<img src="  /on6.png" alt="btn_on" />
 								</div>
 								<div class="swap-off h-6 w-12">
-									<img src=" {base}/off6.png" alt="btn_off" />
+									<img src="  /off6.png" alt="btn_off" />
 								</div>
 							</label>
 						</div>
@@ -1447,10 +1447,10 @@
 									on:change={() => jadwalAktif(4)}
 								/>
 								<div class="swap-on h-6 w-12">
-									<img src=" {base}/on6.png" alt="btn_on" />
+									<img src="  /on6.png" alt="btn_on" />
 								</div>
 								<div class="swap-off h-6 w-12">
-									<img src=" {base}/off6.png" alt="btn_off" />
+									<img src="  /off6.png" alt="btn_off" />
 								</div>
 							</label>
 						</div>
@@ -1547,10 +1547,10 @@
 									on:change={() => jadwalAktif(5)}
 								/>
 								<div class="swap-on h-6 w-12">
-									<img src=" {base}/on6.png" alt="btn_on" />
+									<img src="  /on6.png" alt="btn_on" />
 								</div>
 								<div class="swap-off h-6 w-12">
-									<img src=" {base}/off6.png" alt="btn_off" />
+									<img src="  /off6.png" alt="btn_off" />
 								</div>
 							</label>
 						</div>
