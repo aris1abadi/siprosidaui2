34f637cefd0d16feafba856609d1758578ebcf37
aris1abadi <aris1abadi@gmail.com>
2024-11-17 14:50:11 +0700

    modified:   src/routes/biopest/+page.svelte 	modified:   src/routes/home/+page.svelte 	modified:   src/routes/pestisida/+page.svelte 	modified:   src/routes/setup/+page.svelte 	modified:   src/routes/siram/+page.svelte 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+/*
 import adapter from '@sveltejs/adapter-static';
 
 export default {
@@ -19,4 +20,20 @@ export default {
       entries: ['*'], // Ini memprerender semua halaman
     },
   },
+};
+*/
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
